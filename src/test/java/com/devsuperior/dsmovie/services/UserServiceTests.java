f560bb148113e1e631036b51beac0b096c34e542
PedroZangirolami <pedrozangirolami@gmail.com>
2024-01-15 16:31:20 -0300

    UserServiceTests authenticated test passed

--- src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
@@ -1,14 +1,21 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.entities.UserEntity;
 import com.devsuperior.dsmovie.repositories.UserRepository;
+import com.devsuperior.dsmovie.tests.UserFactory;
 import com.devsuperior.dsmovie.utils.CustomUserUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
@@ -23,17 +30,34 @@ public class UserServiceTests {
 	@Mock
 	private CustomUserUtil customUserUtil;
 
+	private String userNameLogged, userNameNonLoged;
+
 	@BeforeEach
 	void setUp(){
+		userNameLogged = "maria@gmail.com";
+		userNameNonLoged = "bob@gmail.com";
 
+		UserEntity userLogged = UserFactory.createCustomUserEntity(userNameLogged);
+
+		Mockito.when(repository.findByUsername(userNameLogged)).thenReturn(Optional.of(userLogged));
+		Mockito.when(repository.findByUsername(userNameNonLoged)).thenReturn(Optional.empty());
 	}
 
 	@Test
 	public void authenticatedShouldReturnUserEntityWhenUserExists() {
+		Mockito.when(customUserUtil.getLoggedUsername()).thenReturn(userNameLogged);
+
+		UserEntity result = service.authenticated();
+		Assertions.assertNotNull(result);
 	}
 
 	@Test
 	public void authenticatedShouldThrowUsernameNotFoundExceptionWhenUserDoesNotExists() {
+		Mockito.doThrow(ClassCastException.class).when(customUserUtil).getLoggedUsername();
+
+		Assertions.assertThrows(UsernameNotFoundException.class, ()-> {
+			service.authenticated();
+		});
 	}
 
 	@Test
--- src/test/java/com/devsuperior/dsmovie/tests/UserFactory.java
+++ src/test/java/com/devsuperior/dsmovie/tests/UserFactory.java
@@ -8,4 +8,9 @@ public class UserFactory {
 		UserEntity user = new UserEntity(2L, "Maria", "maria@gmail.com", "$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG");
 		return user;
 	}
+
+	public static UserEntity createCustomUserEntity(String username) {
+		UserEntity user = new UserEntity(2L, "Maria", username, "$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG");
+		return user;
+	}
 }
